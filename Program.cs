a421aca9909b247f4c539097dff42668008bf8ef
Artem <lebartigor@gmail.com>
2022-10-15 18:58:22 +0300

    Доделал домашку к 4 семинару и отправляю на проверку

--- Program.cs
+++ Program.cs
@@ -114,6 +114,7 @@ namespace Семинар3дз
         1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
         6, 1, 33 -> [6, 1, 33]*/
 
+        /*
         Console.WriteLine("Введите размер массива");
         int NumArray = int.Parse(Console.ReadLine()!);
 
@@ -129,59 +130,8 @@ namespace Семинар3дз
 
         int[] res = Array(NumArray);
         Console.WriteLine($"[{String.Join(", ", res)}]");
-
-
-
-        // ____________________________СЕМИНАР 5_______________________
-        /***Задача 32:** Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
-        [-4, -8, 8, 2] -> [4, 8, -8, -2]*/
-
-        //___!!!!!____Внизу пример метода для создания массива
-        /*
-        int[] GetArray(int size, int minValue, int maxValue){
-        int[] res = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            res[i] = new Random().Next(minValue, maxValue + 1);
-        }
-        return res;
-        }
         */
 
-        // Задача. Напишите программу меняющую знак каждого элемента массива
-
-        /*
-        int[] array = GetArray(12,-9,9);
-        Console.WriteLine(String.Join(" ", array));
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = array[i] * -1;
-        }
-        
-        Console.WriteLine(String.Join(" ", array));
-
-        */
-
-        /* **Задача 33:** Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-        4; массив [6, 7, 19, 345, 3] -> нет
-        3; массив [6, 7, 19, 345, 3] -> да*/
-
-
-        /*
-        int[] array = GetArray(123,1,500);
-        //Console.WriteLine(String.Join(" ", array));
-              
-            int count = 0;
-            
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i]>9 && array[i]<100)
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine($"Кол-во повторений {count}");
-            */
 
 
          
